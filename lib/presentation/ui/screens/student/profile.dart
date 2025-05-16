83907d7d16e8ca7f04145da075a1e94b813e647c
omnia <omniacis1@gmail.com>
2025-05-16 15:35:49 +0300

    add QR code and signout textbutton

--- lib/presentation/ui/screens/student/profile.dart
+++ lib/presentation/ui/screens/student/profile.dart
@@ -1,13 +1,19 @@
+import 'package:eleaning/core/constant_text.dart';
 import 'package:eleaning/core/helper/color_helper.dart';
+import 'package:eleaning/core/helper/text_style_helper.dart';
 import 'package:eleaning/extensions/navigation_extension.dart';
 import 'package:eleaning/presentation/cubit/profile/profile_cubit.dart';
 import 'package:eleaning/presentation/cubit/profile/profile_state.dart';
+import 'package:eleaning/presentation/cubit/signout/signout_cubit.dart';
 import 'package:eleaning/presentation/cubit/user/user_cubit.dart';
 import 'package:eleaning/presentation/cubit/user/user_state.dart';
+import 'package:eleaning/presentation/ui/screens/auth/login.dart';
 import 'package:eleaning/presentation/ui/screens/student/edit_profile.dart';
+import 'package:eleaning/presentation/ui/widgets/common_widget/custom_elevated_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:sizer/sizer.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -122,27 +128,105 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              "User Name : ${state.user[0].userName}",
-                              style: TextStyle(
-                                fontSize: 14.sp,
-                                fontWeight: FontWeight.bold,
+                            Text.rich(
+                              TextSpan(
+                                text: "${ConstantText.username} : ",
+                                style: TextStyleHelper.textStylefontSize16
+                                    .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      color: ColorHelper.red,
+                                    ),
+                                children: [
+                                  TextSpan(
+                                    text: state.user[0].userName,
+                                    style: TextStyleHelper.textStylefontSize18
+                                        .copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
                               ),
                             ),
-                            Text(
-                              "User Full Name : ${state.user[0].userFullName}",
-                              style: TextStyle(
-                                fontSize: 14.sp,
-                                fontWeight: FontWeight.bold,
+                            SizedBox(height: 2.h),
+                            Text.rich(
+                              TextSpan(
+                                text: "${ConstantText.fullName} :  ",
+                                style: TextStyleHelper.textStylefontSize16
+                                    .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      color: ColorHelper.red,
+                                    ),
+                                children: [
+                                  TextSpan(
+                                    text: state.user[0].userFullName,
+                                    style: TextStyleHelper.textStylefontSize18
+                                        .copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
                               ),
                             ),
-                            Text(
-                              "User Mail : ${state.user[0].userMail}",
-                              style: TextStyle(
-                                fontSize: 14.sp,
-                                fontWeight: FontWeight.bold,
+                            SizedBox(height: 2.h),
+                            Text.rich(
+                              TextSpan(
+                                text: "${ConstantText.email} : ",
+                                style: TextStyleHelper.textStylefontSize16
+                                    .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      color: ColorHelper.red,
+                                    ),
+                                children: [
+                                  TextSpan(
+                                    text: state.user[0].userMail,
+                                    style: TextStyleHelper.textStylefontSize18
+                                        .copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
                               ),
                             ),
+                            SizedBox(height: 2.h),
+                            Text(
+                              ConstantText.shareProfile,
+                              style: TextStyleHelper.textStylefontSize18
+                                  .copyWith(
+                                    color: ColorHelper.red,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                            ),
+                            SizedBox(height: 20),
+                            Center(
+                              child: QrImageView(
+                                data: "",
+                                version: QrVersions.auto,
+                                size: 200.0,
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              ConstantText.scanQRCode,
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: 3.h),
+                            BlocProvider(
+                              create: (context) => SignOutCubit(),
+                              child: TextButton(
+                                onPressed: () {
+                                  context.read<SignOutCubit>().signOut();
+                                  context.pushRemoveUntil(LoginScreen());
+                                },
+                                child: Text(
+                                  ConstantText.logout,
+                                  style: TextStyleHelper.textStylefontSize18
+                                      .copyWith(color: ColorHelper.red),
+                                ),
+                              ),
+                            ),
+                            // CustomElevatedButton(
+                            //   buttonText: ConstantText.logout,
+                            //   onPressedFunction: () {
+                            //     context.read<UserCubit>().signOut();
+                            //   },
+                            //   buttonColor: ColorHelper.red,
+                            //   textColor: ColorHelper.white,
+                            //   widthButton: 40.w,
+                            // ),
                           ],
                         ),
                       ),
