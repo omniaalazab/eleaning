1bb3d1034ae9ba145b3977529ca4bda48b08a352
omnia <omniacis1@gmail.com>
2025-06-02 18:41:37 +0300

    delete secretcode of stripe

--- lib/core/constant_text.dart
+++ lib/core/constant_text.dart
@@ -61,6 +61,5 @@ class ConstantText {
   static const String logout = "Log out";
   static const String stripePublishableKey =
       "pk_test_51RVEgTQTuEnVAVOq1MppPgUyKQxdGWixjboLaPJTTxGgO5gj0Z67Vf6Mu7I2mZ3Ydj50juLPWPKCguJkHOIcaeWx00iWKBP1We";
-  static const String stripeSecretKey =
-      "sk_test_51RVEgTQTuEnVAVOqmAaMXgWFaLjRReRzCPwep30mvFEcfOyARLj7mZKXhToyIDeZj03K2DAXYUOHM4qcsqYB2UbE00nnJ2YLWa";
+  static const String stripeSecretKey = "";
 }
