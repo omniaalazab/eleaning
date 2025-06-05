1c009c97161dade70e43d7ccdaca962f1c3ac127
omnia <omniacis1@gmail.com>
2025-06-05 16:56:30 +0300

    create dio helper

---
+++ lib/data/services/dio_helper.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:eleaning/data/services/stripe_payment/stripe_api.dart';
+
+class DioHelper {
+  static late Dio _dio;
+
+  static Future<Response> post(String baseUrl, Map<String, dynamic> data) {
+    BaseOptions options = BaseOptions(
+      headers: {
+        'Authorization': 'Bearer ${ApiKeys.stripeSecretKey}',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    );
+
+    _dio = Dio(options);
+    return _dio.post(baseUrl, data: data);
+  }
+}
