b1c4d1a0b36d11a4aa5a2a5f342808c137685829
omnia <omniacis1@gmail.com>
2025-06-05 16:56:47 +0300

    add api keys

---
+++ lib/data/services/stripe_payment/stripe_api.dart
@@ -0,0 +1,7 @@
+abstract class ApiKeys{
+static const String stripePublishableKey =
+      "pk_test_51RVEgTQTuEnVAVOq1MppPgUyKQxdGWixjboLaPJTTxGgO5gj0Z67Vf6Mu7I2mZ3Ydj50juLPWPKCguJkHOIcaeWx00iWKBP1We";
+  static const String stripeSecretKey =
+      "sk_test_51RVEgTQTuEnVAVOqmAaMXgWFaLjRReRzCPwep30mvFEcfOyARLj7mZKXhToyIDeZj03K2DAXYUOHM4qcsqYB2UbE00nnJ2YLWa";
+
+}
