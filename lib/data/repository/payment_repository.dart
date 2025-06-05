c266dea92350dbb8295a4bb50715bc48daa0e93f
omnia <omniacis1@gmail.com>
2025-06-05 18:23:54 +0300

    create payment repository

---
+++ lib/data/repository/payment_repository.dart
@@ -0,0 +1,24 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:eleaning/data/services/stripe_payment/payment_manager.dart';
+
+class PaymentRepository {
+  Future<bool> processPayment({
+    required int amount,
+    required String currency,
+  }) async {
+    try {
+      return await PaymentManager.makePayment(amount, currency);
+    } catch (e) {
+      log("Payment processing failed: $e");
+      return false;
+    }
+  }
+
+  Future<void> markCourseAsPaid(String courseId) async {
+    await FirebaseFirestore.instance.collection('courses').doc(courseId).update(
+      {'isPaid': true},
+    );
+  }
+}
